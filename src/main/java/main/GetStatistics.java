5cbc5ce4c53e9dd641194c25ba3d1d45f36b7338
pavserger@gmai.com <pavserger@gmail.com>
2024-02-29 14:44:11 +0300

    29.02.24,15:10, add statistics

---
+++ src/main/java/main/GetStatistics.java
@@ -0,0 +1,90 @@
+package main.searchengine;
+
+import com.github.tsohr.JSONArray;
+import com.github.tsohr.JSONObject;
+import lombok.Data;
+import main.model.*;
+
+import java.util.List;
+
+@Data
+public class  GetStatistics {
+
+    private SiteRepository siteRepository;
+    private PageRepository pageRepository;
+    private LemmaRepository lemmaRepository ;
+    private IndexRepository indexRepository;
+
+
+        public GetStatistics (SiteRepository siteRepository,
+                          PageRepository pageRepository,
+                          LemmaRepository lemmaRepository,
+                          IndexRepository indexRepository) {
+
+        this.siteRepository = siteRepository;
+        this.pageRepository = pageRepository;
+        this.lemmaRepository = lemmaRepository;
+        this.indexRepository = indexRepository;
+    }
+
+      public String getStatisticsData() {
+
+          JSONObject result = new JSONObject();
+          JSONObject statistics = new JSONObject();
+          //  JSONArray ar =new JSONArray();
+          JSONObject total = new JSONObject();
+         //siteRepository.count();
+          total.put("sites",siteRepository.count());
+          total.put("pages",pageRepository.count());
+          total.put("lemmas",lemmaRepository.count());
+          total.put("indexing",true);
+
+          List<Site> sites = siteRepository.findAll();
+
+          JSONArray detals =new JSONArray();
+
+          for (Site site: sites) {
+              JSONObject detal = new JSONObject();
+              detal.put("url",site.getUrl());
+              detal.put("name",site.getName());
+              detal.put("status",site.getType());
+              detal.put("statusTime",site.getStatusTime());
+              detal.put("error",site.getLastError());
+
+              Long i = site.getId();
+              detal.put("pages",pageRepository.findBySite_id(i).size());
+              // many to many  add cod
+              detal.put("lemmas",33);
+              detals.put(detal);
+          }
+/*
+          JSONObject detal = new JSONObject();
+          detal.put("url","http//name");
+          detal.put("name","name");
+
+          detal.put("status","INDEXED");
+          detal.put("statusTime",1600160357);
+          detal.put("error","Ошибка индексации: главная страница сайта недоступна");
+          detal.put("pages",5764);
+          detal.put("lemmas",32115);
+
+          JSONArray detals =new JSONArray();
+
+          detals.put(detal);
+          detals.put(detal);
+
+*/
+
+          statistics.put("total",total);
+          statistics.put("detailed",detals);
+
+          result.put("result",true);
+          result.put("statistics",statistics);
+
+          return result.toString();
+    }
+
+
+
+
+}
